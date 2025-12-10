0005c8dd5735b6225d8409160f46da77f5a9d414
zedddie <zedddiezxc@gmail.com>
2025-12-10 15:02:21 +0100

    feat: project skeleton

---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/zen-mode-hyprland"
---
+++ src/zen-mode-hyprland.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Zen::Mode::Hyprland`
+module Zen::Mode::Hyprland
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
